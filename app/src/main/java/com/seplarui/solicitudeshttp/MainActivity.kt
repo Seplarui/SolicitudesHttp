c79dd347323a01c14660deef20aa297c4ea0cc0d
Sebas <seplarui@gmail.com>
2020-11-01 21:16:31 +0100

    ok

--- app/src/main/java/com/seplarui/solicitudeshttp/MainActivity.kt
+++ app/src/main/java/com/seplarui/solicitudeshttp/MainActivity.kt
@@ -2,10 +2,22 @@ package com.seplarui.solicitudeshttp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bValidarRed = findViewById<Button>(R.id.bValidarRed)
+        bValidarRed.setOnClickListener {
+            //Código para validar la red
+            if(Network.hayRed(this)) {
+                Toast.makeText(this, "Si hay red!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "No hay red!", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
